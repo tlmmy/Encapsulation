e8ac62063348b64b1c3064fce127c9d52ca424a7
trogers8 <trogers8@L11702042765.wctc.msft>
2016-02-04 15:08:51 -0600

    updated StartUp class

--- src/lab1/Startup.java
+++ src/lab1/Startup.java
@@ -19,16 +19,10 @@ public class Startup {
             may need to create a new class that talks to Employee. This does
             not have to be a GUI class.
         */
-        Employee employee = new Employee();
-        employee.firstName = "Peter";
-        employee.lastName = "Piper";
-        employee.ssn = "333-1234";
-        
-        employee.meetDepartmentStaff();
-        employee.meetWithHrForBenefitAndSalryInfo();
-        employee.reviewDeptPolicies();
+       HRManager hr = new HRManager();
+        hr.orientNewEmployee("Tim", "Rogers", "333-33-3333");
 
-        System.out.println("The employee's status is: " + employee.getStatus());
+        System.out.println("The employee's status is: " + hr.getEmployeeStatus());
     }
     
     
